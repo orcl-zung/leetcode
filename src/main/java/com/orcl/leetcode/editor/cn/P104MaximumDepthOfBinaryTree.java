95fbe1fcea70587413841c96f2a6952692b789f9
orcl <577269581@qq.com>
2022-03-07 17:47:00 +0800

    orcl，feat，二叉树最大深度，参考思路来自博客：https://lyl0724.github.io/2020/01/25/1/

---
+++ src/main/java/com/orcl/leetcode/editor/cn/P104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,85 @@
+//给定一个二叉树，找出其最大深度。 
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1137 👎 0
+
+package com.orcl.leetcode.editor.cn;
+
+/**
+ * @description: 二叉树的最大深度
+ * @date: 2022-03-07 17:08:02
+ * @author: orcl
+ */
+public class P104MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P104MaximumDepthOfBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            // 终止条件：当树为空时结束递归，并返回当前深度0
+            if (root == null) {
+                return 0;
+            }
+            // root的左、右子树的最大深度
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+
+            // 返回的时左、右子树的最大深度 +1
+            return Math.max(leftDepth, rightDepth) + 1;
+
+            // 简写方式
+//            return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    class TreeNode {
+        int      val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
